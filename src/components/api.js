725a8aaefd84683d562ca49b0b16ebdba6987426
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-05-07 01:24:54 +0500

    comment removed

--- src/components/api.js
+++ src/components/api.js
@@ -2,11 +2,6 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import '../App.css'
 
-// import Pagination from '@mui/material/Pagination';
-// import Stack from '@mui/material/Stack';
-
-// import { Pagination, Stack } from '@mui/material';
-
 const Api = () => {
 
     let init = 0
@@ -15,18 +10,12 @@ const Api = () => {
     const [searchArr, setSearchArr] = useState([])
     const [searchLimit, setSearchLimit] = useState([])
     const [prev, setPrev] = useState(false)
-    // const [search, setSearch] = useState([])
     const [searchh, setSearchh] = useState(false)
     const [search, setSearch] = useState(false)
-    // const [search, setSearch] = useState([])
     const [filter, setFilter] = useState(false)
     const [sort, setSort] = useState(false)
     const [limit, setLimit] = useState(5)
     const [page, setPage] = useState(1)
-    // const [sortByName, setSortByName] = useState()
-    // const [sortByPrice, setSortByPrice] = useState()
-    // const [filterByName, setFilterByName] = useState()
-    // const [filterByPrice, setFilterByPrice] = useState()
 
     useEffect(() => {
         axios.get(`https://dummyjson.com/products`)
@@ -37,41 +26,29 @@ const Api = () => {
             })
     }, [])
 
-    // const onChange = e => {
-    // const onChange = () => {
     const onChange = (init = 0, limit = 5) => {
-        // console.log('e', e?.target?.value)
-        // axios.get(`https://dummyjson.com/products/search?q=${e?.target?.value}`)
         axios.get(`https://dummyjson.com/products/search?q=${searchh}`)
             .then((res) => {
                 setSearch(true)
                 console.log('search res', res)
-                // setSearch(res?.data?.products)
-                // setPro(res?.data?.products)
                 console.log('search', res?.data?.products);
                 setSearchArr(res?.data?.products)
                 setSearchLimit(res?.data?.products.slice(init, limit))
-                // console.log('searchArr', searchArr);
-                // setSearchLimit([...searchArr].slice(init, limit))
-                // console.log('searchLimit', searchLimit);
-                // setLimit(limit + 5)
-                // console.log('pro', pro)
             })
     }
 
-    const setFilterByNameFunc = (a) => {
-        pro.filter()
-        console.log('setFilter', pro)
-    }
+    const setFilterByNameFunc = () => { }
     const setFilterByPriceFunc = () => { }
 
     const setSortByNameFunc = () => {
+        document.getElementById('inp').value = ''
         setSearch(false)
         let arrTemp = [...pro].sort((a, b) => a.title.localeCompare(b.title))
         setPro(arrTemp)
         console.log('pro', arrTemp);
     }
     const setSortByPriceFunc = () => {
+        document.getElementById('inp').value = ''
         setSearch(false)
         const temp = [...pro].sort((a, b) => {
             return b.price - a.price;
@@ -79,26 +56,14 @@ const Api = () => {
         setPro(temp);
     }
 
-    // const BasicPagination = () => {
-    //     return (
-    //         <Stack spacing={2} >
-    //             <Pagination count={10} />
-    //             <Pagination count={10} color="primary" />
-    //             <Pagination count={10} color="secondary" />
-    //             <Pagination count={10} disabled />
-    //         </Stack >
-    //     );
-    // }
-
     return (
         <div className='main'>
             <input type="text" placeholder='Enter for filter, sort or search' className='input'
-                // onChange={(e) => onChange(e)}
                 onChange={(e) => setSearchh(e?.target?.value)}
+                id={'inp'}
                 autoFocus={true}
             />
             <input type="submit" value={'Search'} className='btn'
-                // onClick={() => onChange()}
                 onClick={onChange}
             />
             {!filter ?
@@ -144,7 +109,6 @@ const Api = () => {
                                 </div>
                             )
                         }) :
-                        // searchArr.map((v, i) => {
                         searchLimit.map((v, i) => {
                             return (
                                 <div key={i} className='div'>
@@ -166,7 +130,6 @@ const Api = () => {
                 {search && <button onClick={() => (init = limit, onChange(init, limit + 5), page < (searchArr.length / 5) ? setPage(page + 1) : null, setPrev(true))}>Next</button>}
             </div>
             <p>{search && page + '/' + (Math.ceil(searchArr.length / 5))}</p>
-            {/* <p>{search && searchArr.length / 5}</p> */}
         </div >
     )
 }
